78b1dba16ee9881f326806588d1eac619f45960d
David Jebing <david.jebing@lise.de>
2022-10-17 15:56:35 +0200

    add vanilla messaging example

--- examples/vanilla/content.js
+++ examples/vanilla/content.js
@@ -1,3 +1,8 @@
 console.log("Content script loaded");
 
 document.body.style.border = "4px solid red";
+
+chrome.runtime.onMessage.addListener((message, sender, _) => {
+  console.log("Received message", message, sender);
+  return true;
+});
